7515cf7cce5632f1d76c27c48db7df6a8c7ac1fe
TerenceLim <terencelimxp@gmail.com>
2017-07-04 10:36:41 +0800

    some angular linking for view2, with bug

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -5,10 +5,42 @@ angular.module('myApp.view2', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'View2Ctrl',
+    controllerAs: 'vm2'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ["$http", "$scope", function($http, $scope) {
+
+	var faculties = ["School of Computing", "School of Engineering", "NUS Business School"];
+	var courses = [["Business Analytics","Computer Science", "Information System", "Information Security"],
+				["Biomedical Engineering","Chemical Engineering","Civil Engineering","Electrical Engineering",
+				"Mechanical Engineering"],
+				["Accountancy","Business Administration"]];
+
+	$scope.faculty = faculties;
+	$scope.courseOptions = [];
+	$scope.getCourseOptions = function(){
+		var key = $scope.faculty.indexOf($scope.faculty);
+		var myNewOptions = courses[key];
+
+		$scope.courseOptions = myNewOptions;
+	};
+	
+	$scope.faculty = [{
+		'name': 'School of Computing',
+		'color': 'Black'
+	}, {
+		'name': 'Faculty of Arts and Social Sciences',
+		'color': 'Black'
+	}, {
+		'name': 'NUS Business School',
+		'color': 'Black'
+	}];
+
+	$scope.cart = {
+		'faculty': $scope.faculty[0]
+	};
+
 
 }]);
